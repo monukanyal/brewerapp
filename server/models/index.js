d1278d229059168819045f3ca3d7bf0abe36099d
monukanyal <monu_kanyal@esferasoft.com>
2018-08-22 14:09:36 +0530

    change to production

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,8 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require('sequelize');
 // var environment = process.env.NODE_ENV || 'development';
-//var environment = 'production';
-var environment = 'development';
+var environment = 'production';
+//var environment = 'development';
 var config = require(__dirname + '/../../config/config.json')[environment];
 
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
